628edf0285a060f86f5cc90672ec70d2e4582aa8
jamilsalim-afk <jamil.salim@gmail.com>
2026-06-17 23:38:18 -0400

    Add normalizarTexto and isFeriado functions

--- js/core.js
+++ js/core.js
@@ -1,1 +1,11 @@
-fdsfa
+function normalizarTexto(txt){
+    return (txt || "")
+        .toString()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "") // remove acentos
+        .replace(/\s+/g, " ") // espaços duplos
+        .trim()
+        .toUpperCase();
+}
+  
+function isFeriado(d){return FERIADOS.includes(d);}
