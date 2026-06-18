4215595a38d3195264fb222618b246904257ddd7
jamilsalim-afk <jamil.salim@gmail.com>
2026-06-17 23:34:40 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,26 @@
-sfad
+const SHEETS={
+INTEGRADO:{id:'1j33kiPqwtzZNuvkBgYDaIiXZvVMY_J0qWAtRfYGdnD8',gid:'1357770092'},
+SUPERIOR:{id:'14ALXZgFIT68ee9ajuIdG63SpGVm0HyTjwp63-J6vRyg',gid:'669887707'},
+PROFESSORES:{id:'1IDjs0oS6lQBGDrL7ja1Ge0vaBdNCNIULDH7J5p89c5s',gid:'1694280391'}
+};
+
+let dadosGlobais = [];
+
+let dadosIntegrado = [];
+let dadosSuperior = [];
+let dadosProfessores = [];
+let listaProfessores = [];
+
+let turmasDaPlanilha = [];
+let semanasAgrupadas = [];
+
+let BASE_GERAL = [];
+let INDEX_PROFESSOR = {};
+let INDEX_TURMA = {};
+
+let semanasIntegrado = {};
+let semanasSuperior = {};
+
+let turmasIntegrado = [];
+let turmasSuperior = [];
+
